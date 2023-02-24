680626857664b4424270cc0fb0db372a26ec4d8d
Alena Evstifeeva <evstifeeva.it@gmail.com>
2023-02-24 15:51:01 +0300

    added hw_3_qa

--- src/test/java/hw_3_qa.java
+++ src/test/java/hw_3_qa.java
@@ -1,2 +1,32 @@
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
+
+
 public class hw_3_qa {
+    @BeforeAll
+    static void beforeAll() {
+        Configuration.browserSize = "1920x1080";
+    }
+
+    @Test
+    public void softAssertionsJunit5() {
+        open("https://github.com/selenide/selenide");
+        $("#wiki-tab").click();
+        $("#wiki-pages-filter").click();
+        $("#wiki-pages-filter").setValue("SoftAssertions");
+        $(byText("SoftAssertions")).shouldBe(Condition.visible).click();
+        $(".markdown-body").shouldHave(text("Using JUnit5 extend test class"));
+
+
+
+        sleep(2000);
+
+    }
+
 }
